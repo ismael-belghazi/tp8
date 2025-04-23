73f24053a11df78fce6f3fa206b758521d2790fd
ismael <ismaelbelghazi@yahoo.com>
2025-04-23 10:14:45 +0200

    Update login.test.js

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,13 +1,23 @@
 import { Selector } from 'testcafe';
 
 fixture`Test Login`
-    .page`http://localhost:8080/public/index.html`;
-
-test('Connexion avec identifiants valides', async t => {
-    await t
-        .typeText('#email', 'test@example.com')
-        .typeText('#password', '123456')
-        .click('button')
-        .expect(Selector('#message').innerText)
-        .eql('Connexion réussie !');
-});
+    .page`http://127.0.0.1:5500/public/index.html`;
+    
+    test('Connexion avec identifiants valides', async t => {
+        await t
+            .typeText('#email', 'test@example.com')
+            .typeText('#password', '123456')
+            .click('button')
+            .expect(Selector('#message').innerText)
+            .eql('Connexion réussie !');
+    });
+    
+    test('Connexion avec identifiants invalides', async t => {
+        await t
+            .typeText('#email', 'invalid@example.com')
+            .typeText('#password', 'wrongpassword')
+            .click('button')
+            .expect(Selector('#message').innerText)
+            .eql('Identifiants invalides.');
+    });
+    

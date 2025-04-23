7049f918ea7bfc1459bd1b5e22632d04c79d3d4d
ismael <ismaelbelghazi@yahoo.com>
2025-04-23 10:27:41 +0200

    change log

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,7 +1,7 @@
 import { Selector } from 'testcafe';
 
 fixture`Test Login`
-    .page`http://127.0.0.1:5500/public/index.html`;
+    .page`http://127.0.0.1:8080`;
     
     test('Connexion avec identifiants valides', async t => {
         await t

5dafbb305602ce37577a3f7da5d13cb2656e973d
ismael-belghazi <155482135+ismael-belghazi@users.noreply.github.com>
2025-04-23 09:59:14 +0200

    Update login.test.js

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,7 +1,7 @@
 import { Selector } from 'testcafe';
 
 fixture`Test Login`
-    .page`http://127.0.0.1:5500/public/index.html`;
+    .page`http://localhost:8080/public/index.html`;
 
 test('Connexion avec identifiants valides', async t => {
     await t
